acd0e708d7905f0d48c981e0ac17720cce8a2f5c
Bruno Pozzebon <brunopozzebon44@gmail.com>
2022-08-20 10:24:52 -0300

    ui updated

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from roboticHand import RoboticHand
 videoPath = "myvideo.mp4"
 videoTitle = "Video"
 pTime = 0
+fontColor = (50, 50, 50)
 
 video = cv2.VideoCapture(videoPath)
 
@@ -25,7 +26,7 @@ while(video.isOpened()):
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(frame, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'FPS:{int(fps)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, fontColor, 2)
 
     results = hands.process(frame)
     if results.multi_hand_landmarks:
@@ -34,10 +35,19 @@ while(video.isOpened()):
             
             if len(landsmarks) == 21:
                 roboticHand = RoboticHand(landsmarks)
-                for id, lm in enumerate(landsmarks):
-                    h, w, c = frame.shape
-                    cx, cy = int(lm.x *w), int(lm.y*h)
-                    cv2.circle(frame, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                angles = roboticHand.getAngles()
+                
+                cv2.putText(frame, f'Little: {round(angles[0], 2)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, fontColor, 2)
+                cv2.putText(frame, f'Indicator: {round(angles[1], 2)}', (20, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, fontColor, 2)
+                cv2.putText(frame, f'Middle finger: {round(angles[2], 2)}', (20, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.5, fontColor, 2)
+                cv2.putText(frame, f'Ring finger: {round(angles[3], 2)}', (20, 130), cv2.FONT_HERSHEY_SIMPLEX, 0.5, fontColor, 2)
+                cv2.putText(frame, f'Thumb finger: {round(angles[4], 2)}', (20, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, fontColor, 2)
+
+                # This is to customize de dots of connectors
+                #for id, lm in enumerate(landsmarks):
+                #    h, w, c = frame.shape
+                #    cx, cy = int(lm.x *w), int(lm.y*h)
+                #    cv2.circle(frame, (cx,cy), 3, (255,0,255), cv2.FILLED)
 
                 mpDraw.draw_landmarks(frame, handLms, mediaPipeHands.HAND_CONNECTIONS)
  
--- roboticHand.py
+++ roboticHand.py
@@ -1,29 +1,29 @@
 from vector import Vector
 from roboticFinger import RoboticFinger
 from utils import Utils
+
 class RoboticHand:
     
     
     def __init__(self, data):
         self.data = data
-        self.setUp()
-
-    def setUp(self):
-        littleFinger = RoboticFinger(self.data[17], self.data[18], self.data[20])
-        indicatorFinger = RoboticFinger(self.data[13], self.data[14], self.data[16])
-        middleFinger = RoboticFinger(self.data[9], self.data[10], self.data[12])
-        ringFinger = RoboticFinger(self.data[5], self.data[6], self.data[8])
-        thumb = RoboticFinger(self.data[1], self.data[2], self.data[4])
-
-
-        angle = littleFinger.getAngleOfFinger()
+        self.littleFinger = RoboticFinger(self.data[17], self.data[18], self.data[20])
+        self.indicatorFinger = RoboticFinger(self.data[13], self.data[14], self.data[16])
+        self.middleFinger = RoboticFinger(self.data[9], self.data[10], self.data[12])
+        self.ringFinger = RoboticFinger(self.data[5], self.data[6], self.data[8])
+        self.thumb = RoboticFinger(self.data[1], self.data[2], self.data[4])
        
-        print(Utils.toDegrees(angle))
+    def getAngles(self):
+        littleFingerAngle = self.littleFinger.getAngleOfFinger()
+        indicatorFingerAngle = self.indicatorFinger.getAngleOfFinger()
+        middleFingerAngle = self.middleFinger.getAngleOfFinger()
+        ringFingerAngle = self.ringFinger.getAngleOfFinger()
+        thumbAngle = self.thumb.getAngleOfFinger()
 
-
-       
-
-        
-
-
-        
+        return [
+            Utils.toDegrees(littleFingerAngle),
+            Utils.toDegrees(indicatorFingerAngle),
+            Utils.toDegrees(middleFingerAngle),
+            Utils.toDegrees(ringFingerAngle),
+            Utils.toDegrees(thumbAngle),
+        ]
